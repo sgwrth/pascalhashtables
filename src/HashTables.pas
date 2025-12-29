2c5fd37d1cf77682e8d1585d79a24bf217eacda6
Andreas Siegwarth <andreassiegwarth@startmail.com>
2025-12-29 21:30:31 +0100

    * fix: DeleteNode() not correctly unlinks node for deletion * change: extracted unlinking logic to separate procedure   UnlinkNodeForDelete() * add: procedures for output (deleted key, key not found)

--- src/HashTables.pas
+++ src/HashTables.pas
@@ -14,5 +14,17 @@ begin
   InitTable(Table);
   AppendNode(Table, 'foo', 42);
   AppendNode(Table, 'foo', 42);
+  AppendNode(Table, 'pas', 123);
+  AppendNode(Table, 'c', 234);
+  AppendNode(Table, 'sh', 345);
+  AppendNode(Table, 'py', 456);
+  AppendNode(Table, 'sql', 567);
+  AppendNode(Table, 'js', 678);
+  AppendNode(Table, 'php', 7);
+  DeleteNode(Table, 'foo');
+  AppendNode(Table, 'bat', 8);
+  AppendNode(Table, 'go', 9);
+  AppendNode(Table, 'cpp', 0);
+  DeleteNode(Table, 'bar');
   DisposeTable(Table);
 end.
